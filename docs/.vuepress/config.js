0e169beef1b859ccce6af2a8bdc0c58ab8d55f60
jiatongshun <jiatongshun@dewinfl.com>
2022-02-12 21:16:04 +0800

    demo分支部分文件重新初始化

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,22 +1,103 @@
 module.exports = {
-    title: 'TosinJia\'s blog',
-    description: "TosinJia个人网站",
-    themeConfig:{
-        nav: [
-            { text: 'Home', link: '/' },
-            {text: '搭建', items:[
-                {text: '搭建', link:'/build/'},         // 以'/'结束，默认读取 README.md
-                {text: '搭建2', link:'/build/README1'}, // 可不写后缀 .md
-            ]},
-            {text:"bing", link:"https://www.bing.com/"},
-        ],
-        sidebar: 'auto',
-        sideBarDepth: 6,
-    },
-    plugins: [
-        [
-            'fulltext-search',
-          ],
+    themeConfig: {
+        // navbar: false,  //使用 themeConfig.navbar 来禁用所有页面的导航栏 https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E5%AF%BC%E8%88%AA%E6%A0%8F%E9%93%BE%E6%8E%A5
 
-    ],
+        //https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E5%AF%BC%E8%88%AA%E6%A0%8F%E9%93%BE%E6%8E%A5
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Guide', link: '/guide/' },   //https://vuepress.vuejs.org/zh/guide/directory-structure.html#%E9%BB%98%E8%AE%A4%E7%9A%84%E9%A1%B5%E9%9D%A2%E8%B7%AF%E7%94%B1
+          
+          { text: 'Foo', link: '/foo/' },
+          { text: 'Bar', link: '/bar/' },
+
+          { text: 'External', link: 'https://bing.com' },
+
+          { text: 'Guide1', link: '/guide/', target:'_blank' },
+          { text: 'External1', link: 'https://bing.com', target:'_self', rel:'' },
+          {
+            text: 'Languages',
+            ariaLabel: 'Language Menu',
+            items: [
+              { text: 'Chinese', link: '/language/chinese/' },
+              { text: 'Japanese', link: '/language/japanese/' }
+            ]
+          },
+          {
+            text: 'Languages1',
+            items: [
+              { text: 'Group1', items: [
+                { text: 'Chinese11', link: '/language/chinese/' },
+                { text: 'Japanese11', link: '/language/japanese/' }                  
+              ] },
+              { text: 'Group2', items: [
+                { text: 'Chinese12', link: '/language/chinese/' },
+                { text: 'Japanese12', link: '/language/japanese/' }                        
+              ] }
+            ]
+          }
+        ],
+
+        // https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E4%BE%A7%E8%BE%B9%E6%A0%8F
+        // sidebar: [
+        //     '/',
+        //     '/page-a',
+        //     ['/page-b', 'Explicit link text'],
+        //     '/guide/',
+        //     '/language/chinese/',
+        //     '/language/japanese/'
+        //   ],
+        // https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E5%B5%8C%E5%A5%97%E7%9A%84%E6%A0%87%E9%A2%98%E9%93%BE%E6%8E%A5
+        sidebarDepth: 5,
+        // https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E6%98%BE%E7%A4%BA%E6%89%80%E6%9C%89%E9%A1%B5%E9%9D%A2%E7%9A%84%E6%A0%87%E9%A2%98%E9%93%BE%E6%8E%A5
+        displayAllHeaders: true, // 默认值：false
+
+        // https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E6%B4%BB%E5%8A%A8%E7%9A%84%E6%A0%87%E9%A2%98%E9%93%BE%E6%8E%A5
+        // activeHeaderLinks: false, // 默认值：true
+
+        // https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E4%BE%A7%E8%BE%B9%E6%A0%8F%E5%88%86%E7%BB%84
+        // sidebar: [
+        //     {
+        //       title: 'Group 1',   // 必要的
+        //       path: '/foo/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        //       collapsable: false, // 可选的, 默认值是 true,
+        //       sidebarDepth: 1,    // 可选的, 默认值是 1
+        //       children: [
+        //         '/'
+        //       ]
+        //     },
+        //     {
+        //       title: 'Group 2',
+        //       children: [ 
+        //         '/language/chinese/',
+        //         '/language/japanese/'
+        //        ],
+        //       initialOpenGroupIndex: -1 // 可选的, 默认值是 0
+        //     }
+        //   ],
+
+        //   https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E5%A4%9A%E4%B8%AA%E4%BE%A7%E8%BE%B9%E6%A0%8F
+        sidebar: {
+            '/foo/': [
+              '',     /* /foo/ */
+              'one',  /* /foo/one.html */
+              'two'   /* /foo/two.html */
+            ],
+      
+            '/bar/': [
+              '',      /* /bar/ */
+              'three', /* /bar/three.html */
+              'four'   /* /bar/four.html */
+            ],
+      
+            // fallback
+            '/': [
+              '',        /* / */
+              'contact', /* /contact.html */
+              'about'    /* /about.html */
+            ]
+          },
+
+        // https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E8%87%AA%E5%8A%A8%E7%94%9F%E6%88%90%E4%BE%A7%E6%A0%8F
+        // sidebar: 'auto',
+      }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,2 +1,36 @@
+---
+# navbar: false   # https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E7%A6%81%E7%94%A8%E5%AF%BC%E8%88%AA%E6%A0%8F
 
-# Hello TosinJia!
+# https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E9%A6%96%E9%A1%B5
+home: true
+heroImage: /hero.png
+heroText: Hero 标题
+tagline: Hero 副标题
+actionText: 快速上手 →
+actionLink: /zh/guide/
+features:
+- title: 简洁至上
+  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
+- title: Vue驱动
+  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
+- title: 高性能
+  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
+# footer: MIT Licensed | Copyright © 2018-present Evan You
+---
+
+<!-- https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E5%AF%8C%E6%96%87%E6%9C%AC-footer -->
+::: slot footer
+MIT Licensed | Copyright © 2021-present [Evan You](https://github.com/yyx990803)
+:::
+
+
+# hello VuePress
+- https://vuepress.vuejs.org/zh/guide/getting-started.html
+    - https://vuepress.vuejs.org/zh/guide/directory-structure.html#%E9%BB%98%E8%AE%A4%E7%9A%84%E9%A1%B5%E9%9D%A2%E8%B7%AF%E7%94%B1
+        - https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E9%A6%96%E9%A1%B5
+
+
+```
+E:\iEnviroment\development\projects\ideaProjects\VueDemo\vuepress-starter>yarn install 
+E:\iEnviroment\development\projects\ideaProjects\VueDemo\vuepress-starter>yarn docs:dev
+```
